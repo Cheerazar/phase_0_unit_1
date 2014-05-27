7707b59a29bc000951118dfb56203884aee5f6fa
Gunnari Auvinen <gauvinen@gmail.com>
2014-05-27 19:00:58 -0400

    Add pseudocode, solution, and reflections to week 3 challenge 4.

--- week_3/4_manipulating_js_objects/my_solution.js
+++ week_3/4_manipulating_js_objects/my_solution.js
@@ -16,9 +16,36 @@ var terah = {
 
 // __________________________________________
 // Write your code below.
+/**
+ * Pseudocode
+ * initialize adam to empty object
+ * add name property to adam and set to string Adam
+ * add spouse property to terah and set to adam object
+ * set terah weight property to 125
+ * remove terah eyeColor property
+ * add spouse property to adam and set to terah object
+ * add children property to terah object and set to empty object
+ * add carson property to terah children property and set to 
+ * object with name property set to string Carson
+ * add carter property to terah children property and set to 
+ * object with name property set to string Carter
+ * add colton property to terah children property and set to 
+ * object with name property set to string Colton
+ * set adam children property to terah children property
+ */
 
 
-
+var adam = {};
+adam['name'] = 'Adam';
+terah['spouse'] = adam;
+terah['weight'] = 125;
+delete terah['eyeColor'];
+adam['spouse'] = terah;
+terah['children'] = {};
+terah.children['carson'] = {'name': 'Carson'};
+terah.children['carter'] = {name: 'Carter'};
+terah.children['colton'] = {name: 'Colton'};
+adam['children'] = terah.children;
 
 
 
@@ -29,31 +56,44 @@ var terah = {
 /**
  *
  * What parts of your strategy worked? What problems did you face?
- * 
+ *
+ * All parts of my strategy worked. The one problem I ran into during this challenge
+ * occured when I didn't read step 7 thoroughly enough. I tried to add a 'carson'
+ * property to terah.children and set it to 'Carson'. I missed the fact that carson
+ * was a property that would be set to an object. Easily fixed once I realized it though.
  * 
  * 
  * What questions did you have while coding? What resources did you find to help you answer them?
- * 
+ *
+ * I couldn't remember the exact syntax to delete a property from an object, so I looked it up 
+ * to get it right and I found the syntax guide on stackoverflow.
  * 
  * 
  * What concepts are you having trouble with, or did you just figure something out? If so, what?
  * 
+ * I didn't have any trouble with any of the concepts in this challenge.
  * 
  * 
  * Did you learn any new skills or tricks?
- * 
- * 
+ *
+ * I was refreshed on how to delete an object property, which is quite useful.
+ *
  * 
  * How confident are you with each of the Learning Competencies?
- * 
+ *
+ * I am very confident in the learning competencies in this challegne.
  * 
  * 
  * Which parts of the challenge did you enjoy?
- * 
+ *
+ * All parts of this challenge were enjoyable. JavaScript is a great language and I really
+ * enjoy programming in it when the opportunity arises.
  * 
  * 
  * Which parts of the challenge did you find tedious?
  *
+ * No parts of this challenge were tedious. It was an excellent reminder to carefully read the 
+ * instructions and also a great refresh on some basic JavaScript syntax.
  *
  * 
  */
