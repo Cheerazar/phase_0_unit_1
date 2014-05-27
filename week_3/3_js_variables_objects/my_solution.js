57e81330cf1faa408c1c02ac23d1aad14e8c1bc1
Gunnari Auvinen <gauvinen@gmail.com>
2014-05-27 18:05:48 -0400

    Add pseudocode, solutions, and reflections to week 3 challenge 3.

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -3,22 +3,61 @@
 // __________________________________________
 // Write your code below.
 
+// initialize secretNumber to 7
 
+var secretNumber = 7;
 
+// initialize password to 'just open the door'
 
+var password = 'just open the door';
 
+// initialize allowedIn to false
 
+var allowedIn = false;
+
+// initialize array members with John as first element and Mary as fourth
+var members = ['John', undefined, undefined, 'Mary'];
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
 // 
-// 
-// 
-// 
-// 
-// 
-
+/**
+ * What parts of your strategy worked? What problems did you face?
+ * 
+ * All parts of my strategy worked. I laid out the pseudocode and then translated that into
+ * JavaScript. I didn't face any problems in this challegne. 
+ * 
+ * What questions did you have while coding? What resources did you find to help you answer them?
+ *
+ * I didn't have any questions, nor did I need any extra resources during this challenge.
+ * 
+ * 
+ * What concepts are you having trouble with, or did you just figure something out? If so, what?
+ *
+ * This challenge didn't have any concepts that were unfamiliar to me.
+ * 
+ * 
+ * Did you learn any new skills or tricks?
+ *
+ * I didn't learn any skills or tricks for this particular challenge.
+ * 
+ * 
+ * How confident are you with each of the Learning Competencies?
+ *
+ * I am very confident that I understand all of the Learning Compentencies in this challenge.
+ * 
+ * 
+ * Which parts of the challenge did you enjoy?
+ * 
+ * Even though this particular challenge was relatively easy for me, I still enjoyed getting the 
+ * correct output from node.
+ * 
+ * Which parts of the challenge did you find tedious?
+ *
+ * No parts of the challenge were tedious. 
+ * 
+ */
 
 // __________________________________________
 // Test Code:  Do not alter code below this line.
