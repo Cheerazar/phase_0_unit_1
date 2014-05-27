392676f096db9bb33b8e26ad1618740c8409e93e
Gunnari Auvinen <gauvinen@gmail.com>
2014-05-27 13:48:25 -0400

    Add pseduocode and solution to first two missions.

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,16 +1,81 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge [by myself]
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
  
+// Rescue Mission
+// Pseudocode
+// move down x2
+// move right
+// move up x2
+// move right x2
+// move down
+// attack!
+
+this.moveDown();
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.attackNearbyEnemy();
+
+// Grab the Mushroom 
+// Pseudocode
+// move up
+// move right
+// move left
+// move up
+// attack!
+
+this.moveUp();
+this.moveRight();
+this.moveLeft();
+this.moveUp();
+this.attackNearbyEnemy();
  
+// Drink Me
+// Pseudocode
+
  
+// Taunt the Guards
+// Pseudocode
+
  
+// It's a Trap
+// Pseudocode
+
  
- 
- 
+// Break the Prison
+// Pseudocode
+
+
+
+// Taunt
+// Pseudocode
+
+
+
+// Cowardly Taunt
+// Pseudocode
+
+
+
+// Commanding Followers
+// Pseudocode
+
+
+
+// Mobile Artillery
+// Pseudocode
+
+
+
+
  
 // Reflection:
 // Write your reflection here.
