fee2ed5b768feafd12e0f1dca9ffe566b4f04577
Gunnari Auvinen <gauvinen@gmail.com>
2014-05-27 12:51:27 -0400

    Add user stories to gc1_user_stories.js

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -1,3 +1,28 @@
 // My role in the group is:
+// Translate tests into user stories.
+
 
 // Here is my part of the challenge:
+
+/**
+ *
+ * Story 1
+ * I want to be able to take a group of numbers and add them all together with a program named sum.
+ * sum should work with any amount of numbers. sum should provide me with the result after completing.
+ * 
+ */
+
+
+/**
+ * Story 2
+ * I want to be able to get the average of a group of numbers with a program named mean.
+ * mean should work with any amount of numbers. mean should provide me with the result after completing.
+ * 
+ */
+
+/**
+ * Story 3
+ * I want to be able to find the value directly in the middle of a group of numbers with a program named median.
+ * median should work with any amount of numbers, either an even or odd amount.
+ * median should provide me with the result after completing. 
+ */
