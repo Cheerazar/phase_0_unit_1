330207ed859e6939f58cc5da5cefdfc2970705e8
Gunnari Auvinen <gauvinen@gmail.com>
2014-05-29 15:56:44 -0400

    Add challenge 5 mission statement and initial pseudocode for initialize function

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -1,16 +1,59 @@
 // This is a solo challenge
 
 // Your mission description:
-// 
-// 
-// 
-
+/**
+ * Mission Description
+ *
+ * Move the colored square from the start to the finish.
+ * Along the way you'll need to change the color of your square to 
+ * match the color of the square at the finish. You change the 
+ * color of your square by moving it on top of another square and 
+ * combinging the colors to get a new color. Once your square matches
+ * the color of the finish square, you'll need to move it there to
+ * finish the level
+ * 
+ */
 // Pseudocode
 // 
-// 
-// 
-// 
-// 
+/**
+ *	canvas width: 600px, height: 300px 
+ * 	each square 30px x 30px, and each movement is the width
+ *
+ * 	intialize object color1 with properties point1, point2, point3, point4
+ * 
+ *  define function initialize
+ *  	if canvas is supported by browser
+ *  		set canvas context to '2d'
+ *  		translate canvas context to cartesean coord system
+ *    	draw 1px outline around canvas perimeter
+ *
+ * 			draw top course line that starts from (0, 165), (180, 165), (180, 225), (480, 225), (480, 165), (600, 165)
+ *    	draw low course line that starts from (0, 135), (180, 135), (180, 75), (480, 75), (480, 135), (600, 135)
+ *     	draw and fill 240 by 90 rectangle, start at (210, 105), (210, 195), (450, 195), (450, 105), end at (210, 105)
+ *    	draw and fill 30 by 30 finish square at (570, 135), (570, 165), (600, 165), (600, 135), (570, 135)
+ *    	draw and fill 30 by 30 start square at (0, 135), (0, 165), (30, 165), (30, 135), (0, 135)
+ *    	draw and fill 30 by 30 color 1 square at (285, 75), (285, 105), (315, 105), (315, 75), (285, 75)
+ *    	draw and fill 30 by 30 color 2 square at (285, 195), (285, 225), (305, 225), (305, 195), (285, 195)
+ *     	
+ *      save canvas
+ * 		else
+ * 			Add an H1 to the section informing the user of requirement for a more up to date browser.
+ * 		end if
+ *  end initialize
+ *
+ * 	document ready
+ *  
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * 	end
+ *
+ * 
+ */
 
 // Initial Code
 
