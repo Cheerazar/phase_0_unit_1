80e9682cf469957800932ac3cf2ea14173fa2984
Gunnari Auvinen <gauvinen@gmail.com>
2014-05-30 20:25:04 -0400

    Add reflection to week 3 gps challenge.

--- week_3/9_gps1_2/our_solution.js
+++ week_3/9_gps1_2/our_solution.js
@@ -82,3 +82,51 @@ var jimCarrey = new Client('Jim Carrey', 52, '...So you\'re telling me there\'s 
 jimCarrey.lastCom();
 
 //  6.  Your Reflection:
+/**
+ * What parts of your strategy worked? What problems did you face?
+ * 
+ * All parts of our strategy worked. Here and there we faced some syntax issues, but they were quickly resolved.
+ * 
+ * 
+ * 
+ * What questions did you have while coding? What resources did you find to help you answer them?
+ *
+ * I had a few quetions when coding the prototype extension that we used in the bonus. I used some of my own 
+ * code that I had used earlier that day to remind myself of the structure of the extension. 
+ * 
+ * 
+ * 
+ * What concepts are you having trouble with, or did you just figure something out? If so, what?
+ *
+ * This challenge didn't have any troubling concepts. The use of a constructor and then extending a prototype 
+ * were both nice exercises. Getting more familiar with JavaScript and programming concepts in general are
+ * great exercises.
+ * 
+ *
+ * 
+ * Did you learn any new skills or tricks?
+ *
+ * I didn't learn any new tricks in this challenge, but I did get the chance to work with my understanding
+ * of JavaScript constructors and prototypes. They are both concepts I'm familiar with, yet they aren't
+ * something that I use every day. 
+ *
+ *
+ * 
+ * How confident are you with each of the Learning Competencies?
+ *
+ * I am very confident with the learning competencies for this challenge. 
+ *
+ * 
+ * Which parts of the challenge did you enjoy?
+ *
+ * All parts of this challenge were enjoyable. It was another coding learning experience, which is very
+ * welcome with me.
+ *
+ * 
+ * Which parts of the challenge did you find tedious?
+ *
+ * The tedious part of the challenge was typing in the object literals for clients 3-5. It was an excellent exercise 
+ * though, as it demonstrated that doing that all the time would drive you up a wall. Thus instead of 
+ * defining an object literal each time, you would instead lean towards a constructor to make things easier.
+ * 
+ */
