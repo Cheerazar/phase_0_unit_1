4b18251399f283ef54ad637c630cdc906a2f07e7
Gunnari Auvinen <gauvinen@gmail.com>
2014-05-31 22:16:38 -0400

    Add reflection to week 3 group challenge.

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -26,3 +26,60 @@
  * median should work with any amount of numbers, either an even or odd amount.
  * median should provide me with the result after completing. 
  */
+
+// Reflection
+
+/**
+ * What parts of your strategy worked? What problems did you face?
+ *
+ * In the end all parts of my strategy worked. The JavaScript functions that I described in user
+ * stories were translated very well into pseudocode, code, and refactored code. Finally they were
+ * run through the tests and passed successfully. 
+ *
+ * The problem that I ran into was trying to describe what the function was supposed to do without
+ * getting too technical. It was difficult to not use words like array while writing the user stories.
+ * 
+ *
+ * 
+ * What questions did you have while coding? What resources did you find to help you answer them?
+ *
+ * I didn't do any coding in this challenge, so I didn't have any questions there.
+ *
+ *
+ * 
+ * What concepts are you having trouble with, or did you just figure something out? If so, what?
+ *
+ * I didn't have any trouble with the concepts as I was writing the user stories, nor looking at 
+ * the final code. The functions that I created my stories from were easy to understand. 
+ *
+ *
+ * 
+ * Did you learn any new skills or tricks? 
+ *
+ * I didn't really learn any new skills or tricks with this challenge.
+ *
+ * 
+ * 
+ * How confident are you with each of the Learning Competencies?
+ *
+ * I am very confident in the Learning Competencies for this challenge.
+ * 
+ *
+ * 
+ * Which parts of the challenge did you enjoy?
+ *
+ * I enjoyed seeing that my teammates were able to interpret my user stories and create working
+ * code. That was quite rewarding.
+ *
+ *
+ * 
+ * Which parts of the challenge did you find tedious?
+ *
+ * This was a rather bland challenge for me. The user stories weren't that difficult to create.
+ * Nothing was tedious in this challenge. I just didn't really benefit from this particular 
+ * challenge. As the person who wrote the user stories I had the easiest job. Running the 
+ * tests through node isn't hard if the code works. Maybe it would have been more difficult
+ * if things hadn't run smoothly. 
+ *
+ *
+ */
